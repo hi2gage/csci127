9ce2fd58ed6d5032b57653f3f0d89fa3d1f5d490
Gage Halverson <coolgage@Gages-MacBook-Pro-2.local>
2019-03-11 21:12:40 -0600

    Done with the Lab!

--- Labs/lab_9/prob_2.py
+++ Labs/lab_9/prob_2.py
@@ -4,10 +4,15 @@ class Appliance():
 
 class Refrigerator(Appliance):
     def __init__(self, manufacturer, refrigerant):
+        Appliance.__init__(self, manufacturer)
         self.refrigerant = refrigerant
     def __str__(self):
-        return self.refrigerant
+        print_str = self.refrigerant + " is the refrigerant (cooling agent) used"
+        return print_str
 
 # The missing code goes here but write it below.
 my_refrigerator = Refrigerator("Samsung", "R134a")
 print(my_refrigerator)
+# R134a is the refrigerant (cooling agent) used
+
+#print(help(Refrigerator))
