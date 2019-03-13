0b971692d21a1b45bf3031809dc25ed02b52229b
Gage Halverson <coolgage@Gages-MacBook-Pro-2.local>
2019-03-13 16:13:10 -0600

    Finished with Lab 9 and program 4

--- Labs/lab_9/Gage-Halverson-Lab9.py
+++ Labs/lab_9/Gage-Halverson-Lab9.py
@@ -1,3 +1,27 @@
+# Problem 1
+
+score_differences = {}
+score_differences["October 7, 2017"] = 8
+score_differences["October 14, 2017"] = -12
+score_differences["October 21, 2017"] = 3
+
+# The missing code goes here but write it below. Assume that every game results in either a win or a loss.
+wins = 0
+losses = 0
+dic_list = list(score_differences.values())
+for num in dic_list:
+    if num > 0:
+        wins +=1
+    else:
+        losses +=1
+
+# End of Code added
+
+print(wins, "wins -", losses, "losses")
+
+
+# Problem 2
+
 class Appliance():
     def __init__(self, manufacturer):
         self.manufacturer = manufacturer
@@ -15,5 +39,3 @@ class Refrigerator(Appliance):
 my_refrigerator = Refrigerator("Samsung", "R134a")
 print(str(my_refrigerator))
 # R134a is the refrigerant (cooling agent) used
-
-#print(help(Refrigerator))
--- Labs/lab_9/prob_1.py
+++ Labs/lab_9/prob_1.py
@@ -1,19 +1,7 @@
-#Question One. 50 points. Supply the missing Python code below.
-#The scores_differences dictionary contains the difference between
-#Montana State’s football score and its opponent’s football score on
-#a given date. For example, on October 21, 2017, Montana State scored
-#27 points and Northern Colorado scored 24 points, so the difference
-#is 3. In the example below, the output 2 wins – 1 losses should be printed.
-#The solution should be of high quality and work correctly regardless of how
-#many entries are in the dictionary. Comments are not necessary.
-
-
 score_differences = {}
 score_differences["October 7, 2017"] = 8
 score_differences["October 14, 2017"] = -12
 score_differences["October 21, 2017"] = 3
-score_differences["October 25, 2017"] = -12
-score_differences["October 21, 2018"] = 4
 
 # The missing code goes here but write it below. Assume that every game results in either a win or a loss.
 
